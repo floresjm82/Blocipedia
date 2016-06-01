863b02433cfaacd3199286432db8d49fe6ca46ec
Jorge <floresjmjr@gmail.com>
2016-06-01 02:10:31 -0400

    included javascript snippet - Analytics

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,16 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+var analytics = {};
+analytics.report = function(Sale){
+  var event = {event: { name: Sale }};
+
+  var request = new XMLHttpRequest();
+
+  request.open("POST", "http://localhost:3000/api/events", true);
+
+  request.setRequestHeader('Content-Type', 'application/json');
+
+  request.send(JSON.stringify(event));
+}
